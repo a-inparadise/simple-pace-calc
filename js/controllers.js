7e5f0f9e9a54fb49460ed78860c1510fd0d9aac2
Aaron Johnson <aaron.c.johnson@gmail.com>
2014-12-11 16:08:02 -0500

    syntax formatting. little shit.

--- js/controllers.js
+++ js/controllers.js
@@ -2,39 +2,39 @@ var paceApp = angular.module('paceApp', []);
 
 paceApp.controller('calcCtrl', function ($scope) {
 	$scope.paceMin = "00";
-	$scope.paceSecs = "00";
+	$scope.paceSec = "00";
 	$scope.dist = 0;
 	$scope.unit = "Miles";
 	$scope.unitStr = "mile";
 	$scope.timeHrs = "hrs";
 	$scope.timeMin = "min";
-	$scope.timeSecs = "secs";
+	$scope.timeSec = "sec";
 
 	$scope.calculate = function() {
 		// don't calculate until there's a distance
 		if ($scope.dist > 0) {
 			var hours = $scope.timeHrs;
 			var min = $scope.timeMin;
-			var secs = $scope.timeSecs;
+			var sec = $scope.timeSec;
 
 			// do some error checking for NaN
 			if (isNaN(hours)) hours = 0;
 			if (isNaN(min)) min = 0;
-			if (isNaN(secs)) secs = 0;
+			if (isNaN(sec)) secs = 0;
 
-			var totalSecs = (hours * 3600) + (min * 60) + (secs * 1);
+			var totalSec = (hours * 3600) + (min * 60) + (sec * 1);
 
-			var pace = (totalSecs / $scope.dist) / 60;
+			var pace = (totalSec / $scope.dist) / 60;
 
 			$scope.paceMin = Math.floor(pace);
-			$scope.paceSecs = Math.floor((pace - $scope.paceMin) * 60);
+			$scope.paceSec = Math.floor((pace - $scope.paceMin) * 60);
 
-			if ($scope.paceSecs < 10) $scope.paceSecs = "0" + $scope.paceSecs;
+			if ($scope.paceSec < 10) $scope.paceSec = "0" + $scope.paceSec;
 
 			$scope.unitStr = $scope.unit.substring(0, ($scope.unit.length - 1)).toLowerCase();
 		} else {
 			$scope.paceMin = "00";
-			$scope.paceSecs = "00";
+			$scope.paceSec = "00";
 		}
 	};
 
