3cc2ae8dbd7376cdfdc298af3e8ba0cc2347457b
Aaron Johnson <aaron.c.johnson@gmail.com>
2014-12-17 10:44:05 -0500

    cleaned up html markup, cleaned up css.

--- js/controllers.js
+++ js/controllers.js
@@ -6,55 +6,52 @@ paceApp.controller('calcCtrl', function ($scope) {
 	$scope.dist = 0;
 	$scope.unit = "Miles";
 	$scope.unitStr = "mile";
-	$scope.timeHrs = "hh";
-	$scope.timeMin = "mm";
-	$scope.timeSec = "ss";
 
-	$scope.rangeLbls = [];
-	var ranges = {
-		"5k" : {
+	$scope.guesses = [];
+	$scope.ranges = [
+		{
 			"t" : "5k",
 			"d" : 3.1,
 			"l" : 840,
 			"h" : 3600
 		},
-		"5m" : {
+		{
 			"t" : "5m",
 			"d" : 5,
 			"l" : 1500,
 			"h" : 6000
 		},
-		"8k" : {
+		{
 			"t" : "8k",
 			"d" : 4.97,
 			"l" : 1500,
 			"h" : 6000
 		},
-		"10k" : {
+		{
 			"t" : "10k",
 			"d" : 6.2,
 			"l" : 1860,
 			"h" : 7500
 		},
-		"10m" : {
+		{
 			"t" : "10m",
 			"d" : 10,
 			"l" : 3000,
 			"h" : 12000
 		},
-		"13p1" : {
+		{
 			"t" : "13.1m",
 			"d" : 13.1,
 			"l" : 3900,
 			"h" : 15720
 		},
-		"26p2" : {
+		{
 			"t" : "26.2m",
 			"d" : 26.2,
 			"l" : 7860,
 			"h" : 31440
 		}
-	}
+	]
 
 	$scope.applyDistance = function(d) {
 		$scope.dist = d;
@@ -74,9 +71,9 @@ paceApp.controller('calcCtrl', function ($scope) {
 		var totalSec = (hours * 3600) + (min * 60) + (sec * 1);
 
 		// try to guess at the distance
-		$scope.rangeLbls = [];
-		$.each(ranges, function(i, v) {
-			if (totalSec >= v.l && totalSec <= v.h) $scope.rangeLbls.push(v);
+		$scope.guesses = [];
+		$.each($scope.ranges, function(i, v) {
+			if (totalSec >= v.l && totalSec <= v.h) $scope.guesses.push(i);
 		});
 
 		// don't calculate until there's a distance
@@ -105,7 +102,7 @@ paceApp.controller('calcCtrl', function ($scope) {
 		}
 	};
 
-	$("#raceRow").removeClass("hide");
-	$("#orRow").removeClass("hide");
-	$("#paceRow").removeClass("hide");
+	// now that angular is loaded show hidden elements
+	// this is to prevent ugly screen flicker on inital page load
+	$(".row").removeClass("hide");
 });
